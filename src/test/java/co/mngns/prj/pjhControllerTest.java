949aac10582bfae72ec139e139e42d3fa2108238
YD05-19\admin <jhyeon9108@gmail.com>
2021-10-29 15:28:30 +0900

    541654

---
+++ src/test/java/co/mngns/prj/pjhControllerTest.java
@@ -0,0 +1,23 @@
+package co.mngns.prj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import co.mngns.prj.svc.service.ReserListService;
+import co.mngns.prj.svc.vo.ReserListVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/*-context.xml")
+public class pjhControllerTest {
+	@Autowired
+	ReserListService reser;
+
+	@Test
+	public void test1() {
+		ReserListVO rlist = new ReserListVO();
+		rlist.setReser_loc("중구");
+	}
+}
