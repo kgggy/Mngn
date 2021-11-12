81a817a6e2b13af5d5d80f3aa36a14bfc1b7bd05
DESKTOP-NS0NESE\admin <missjh96@gmail.com>
2021-11-12 16:53:06 +0900

    20211112_jhjh

--- src/main/java/co/mngns/prj/pet/vo/PetVO.java
+++ src/main/java/co/mngns/prj/pet/vo/PetVO.java
@@ -4,16 +4,16 @@ import lombok.Data;
 
 @Data
 public class PetVO {
-	private Integer pet_id;
-	private Integer knd;
+	private int pet_id;
+	private int knd;
 	private String name;
 	private String gen;
-	private Integer age;
-	private Integer wgt;
-	private Integer oper;
+	private int age;
+	private int wgt;
+	private int oper;
 	private String memo;
-	private Integer file_no;
-	private Integer client_id;
+	private int file_no;
+	private int client_id;
 	private String species;
 	
 	private String gen_wm;
